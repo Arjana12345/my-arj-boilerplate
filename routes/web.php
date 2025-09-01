e084bd7c72636341177fab6cb4ef910ee2f72022
Arjana <arjana99patel@zohomail.in>
2025-09-01 23:19:48 +0530

    home page route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); ## defasult welcome page by laravel
+});
+
+
+Route::get('/home', function () {
+    return view('home/index');
 });
