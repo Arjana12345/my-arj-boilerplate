07fb2e7bcb3636b190b22d1c9753776205267a74
Arjana <arjana99patel@zohomail.in>
2025-09-02 22:31:48 +0530

    Routes - passing array to view

--- resources/views/home/people.blade.php
+++ resources/views/home/people.blade.php
@@ -14,7 +14,21 @@
         
     </head>
     <body>
-        <h1>My home - {{$name}}</h1>
+        <h1>My home</h1>
+
+        <table border ="1">
+            <tr><th>Name</th><th>Area</th></tr>
+            @foreach($people as $this_candidate)
+            <tr>
+                <td>
+                    <a href="/home/people/{{$this_candidate['id']}}">
+                    {{$this_candidate['name']}}</a>
+                </td>
+                <td>{{$this_candidate['area']}}</td>
+               
+            </tr>
+            @endforeach
+        </table>
         
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,37 @@ Route::get('/', function () {
     return view('welcome'); ## defasult welcome page by laravel
 });
 
+/*
+Route::get('/home', function () {
+    return view('home/index');   ## index is file inside home folder
+});
 
 Route::get('/home', function () {
-    return view('home/index');
+    return view('home.index', ['name' => 'Arjana']);
+});
+*/
+
+# pass array
+Route::get('/home', function () {
+    return view('home.index', ['name' => 'Arjana']);
+});
+
+
+# pass array
+Route::get('/home/people', function () {
+    $data = [
+        ["id" => '1',"name" => "Arjana", 'area' => "php"],
+        ["id" => '2',"name" => "suniti", 'area' => "php"]
+
+    ];
+    return view('home.people', ['people' => $data]);
+});
+
+Route::get('/home/people/', function () {
+    $data = [
+        ["id" => '1',"name" => "Arjana", 'area' => "php"],
+        ["id" => '2',"name" => "suniti", 'area' => "php"]
+
+    ];
+    return view('home.people', ['people' => $data]);
 });
