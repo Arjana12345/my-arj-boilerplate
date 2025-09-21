c5186ea9417568851ab4fc17b0dd9427b5a7d724
Arjana <arjana99patel@zohomail.in>
2025-09-21 21:36:16 +0530

    External css

--- resources/views/companies_type/index.blade.php
+++ resources/views/companies_type/index.blade.php
@@ -8,26 +8,26 @@
             </div>
         @endif
     </div>
-    <table style="border: 1px solid black; border-collapse: collapse;">
-        <tr style="border: 1px solid black; padding: 8px;">
-            <th style="border: 1px solid black; padding: 8px;">Name</th>
-            <th style="border: 1px solid black; padding: 8px;">Description</th>
-            <th style="border: 1px solid black; padding: 8px;">Edit</th>
-            <th style="border: 1px solid black; padding: 8px;">Delete</th>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
         @foreach($companies_type->all() as $this_company)
-            <tr style="border: 1px solid black; padding: 8px;">
-                <td style="border: 1px solid black; padding: 8px;">{{$this_company->name}}</td>
-                <td style="border: 1px solid black; padding: 8px;">{{$this_company->description}}</td>
+            <tr>
+                <td>{{$this_company->name}}</td>
+                <td>{{$this_company->description}}</td>
                 <!-- 
                 pass array 
                 -->
-                <td style="border: 1px solid black; padding: 8px;">
+                <td>
                     <a href="{{route('companies_type.update', ['company_data' => $this_company->id])}}">
                         Edit
                     </a>
                 </td>
-                <td style="border: 1px solid black; padding: 8px;">
+                <td>
                 <form method="post" action="{{route('companies_type.destroy', [ 'company_data' => $this_company->id ])}}">
                     @csrf
                     @method('delete')
