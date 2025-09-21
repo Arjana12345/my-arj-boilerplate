1948265bf7de8ff70bb8ae4dba4460c04d6a7534
Arjana <arjana99patel@zohomail.in>
2025-09-21 21:48:39 +0530

    route group exaple for crud

--- routes/web.php
+++ routes/web.php
@@ -40,9 +40,16 @@ Route::get('/home/people/{id}', function ($id) {
 ####################
 # CRUD route
 ####################
-Route::get('/companies_type', [CompaniesTypeController::class,'index'])->name('companies_type.index');
-Route::get('/companies_type/create', [CompaniesTypeController::class,'create'])->name('companies_type.create');
-Route::post('/companies_type', [CompaniesTypeController::class,'store'])->name('companies_type.store');
-Route::get('/companies_type/{company_data}/update', [CompaniesTypeController::class,'update'])->name('companies_type.update');
-Route::put('/companies_type/{company_data}/update_store', [CompaniesTypeController::class,'update_store'])->name('companies_type.update_store');
-Route::delete('/companies_type/{company_data}/destroy', [CompaniesTypeController::class,'destroy'])->name('companies_type.destroy');
+
+#########################
+## Route group
+##########################
+Route::group(['prefix' => '/companies_type'],function(){
+Route::get('/', [CompaniesTypeController::class,'index'])->name('companies_type.index');
+Route::get('/create', [CompaniesTypeController::class,'create'])->name('companies_type.create');
+Route::post('/', [CompaniesTypeController::class,'store'])->name('companies_type.store');
+Route::get('/{company_data}/update', [CompaniesTypeController::class,'update'])->name('companies_type.update');
+Route::put('/{company_data}/update_store', [CompaniesTypeController::class,'update_store'])->name('companies_type.update_store');
+Route::delete('/{company_data}/destroy', [CompaniesTypeController::class,'destroy'])->name('companies_type.destroy');
+
+});
